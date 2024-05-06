b077ac0eecef2b2a01a9c4ff26498b08aa8b61e0
bandita.eu <laandro3@gmail.com>
2024-05-06 13:09:01 +0200

    Update App.js

--- App.js
+++ App.js
@@ -36,52 +36,68 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Coins: {coins}</Text>
+      <Text style={[styles.coin_display,styles.text_center]}>Coins: {coins}</Text>
+      {machineCount>=1 ? (<Text style={[styles.text_center]}>Machine(s) generating {machineCount} coin every 2 seconds</Text>):(<Text style={[styles.text_center]}>You don't have any machines. Earn 10 coins to buy one!</Text>)}
+      <View style={styles.inner_container}>
+      <TouchableOpacity style={[styles.button,styles.button_enabled]} onPress={handleClick}>
+        <Text style={[styles.text_button,styles.text_center]}>Click to Earn Coins</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, styles.button_disabled, coins>=10 && (styles.button_enabled)]} onPress={buyMachine}>
+        <Text style={[styles.text_button,styles.text_center]}>Buy Machine for 10 Coins</Text>
+      </TouchableOpacity>
+      </View>
       <TouchableOpacity style={styles.button_error} onPress={removeCoins}>
         <Text>Remove coins</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.button_error} onPress={removeMachineCount}>
         <Text>Remove machines</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={handleClick}>
-        <Text>Click to Earn Coins</Text>
-      </TouchableOpacity>
-      {coins >= 10 && (
-        <TouchableOpacity style={styles.button} onPress={buyMachine}>
-          <Text>Buy Machine for 10 Coins</Text>
-        </TouchableOpacity>
-      )}
-      {coins < 10 && (
-        <TouchableOpacity style={styles.button_disabled}>
-          <Text>Buy Machine for 10 Coins</Text>
-        </TouchableOpacity>
-      )}
-      {machineCount>=1 && <Text>Machine(s) generating {machineCount} coin every 2 seconds</Text>}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
+    backgroundColor:'white',
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop:40,
+  },
+  inner_container: {
+    margin:5,
+    backgroundColor:'#12b0b0',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    borderRadius:15
   },
   button: {
-    backgroundColor: 'lightblue',
-    padding: 10,
-    marginVertical: 10,
+    margin:10,
+    padding: 20,
+    width:'95%',
+    borderRadius:15
   },
   button_error: {
     backgroundColor: 'pink',
     padding: 10,
     marginVertical: 10,
   },
-  button_disabled: {
-    backgroundColor: 'lightgray',
-    padding: 10,
-    marginVertical: 10,
+  button_enabled: {
+    backgroundColor: '#ff914d'
   },
+  button_disabled: {
+    backgroundColor: 'lightgray'
+  },
+  coin_display:{
+    padding:20,
+    fontSize:25
+  },
+  text_button:{
+    fontSize:20,
+  },
+  text_center:{
+    textAlign:'center'
+  }
 });
 
 export default App;
